cf890d9933ad70d11c539f6118c9d6319d1f94e2
PegahM <pegahh.monzeri@gmail.com>
2022-04-17 09:18:59 +0430

    Added Synonym Component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,21 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning);
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <h5> {definition.definition}</h5>
             <p>
+              <strong>Defenition:</strong> {definition.definition}
+              <br />
+              <strong>Example:</strong>
               <em> {definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
